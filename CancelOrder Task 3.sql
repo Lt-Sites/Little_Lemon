d824f7f7d055e113ea1acabc79a2f95f5f3d77fa
Lt-Sites <42856882+Lt-Sites@users.noreply.github.com>
2023-11-22 22:55:59 -0600

    Update CancelOrder Task 3.sql

--- CancelOrder Task 3.sql
+++ CancelOrder Task 3.sql
@@ -1,3 +1,16 @@
-CREATE PROCEDURE CancelOrder(IN OrderID INT)
-DELETE FROM orders;
+CREATE PROCEDURE CancelOrder(IN p_OrderID INT)
+BEGIN
+    DECLARE error_message VARCHAR(255);
+
+    -- Verificar si la orden existe antes de intentar cancelarla
+    IF NOT EXISTS (SELECT 1 FROM orders WHERE order_id = p_OrderID) THEN
+        SET error_message = 'La orden no existe.';
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = error_message;
+    ELSE
+        -- Eliminar la orden específica
+        DELETE FROM orders WHERE order_id = p_OrderID;
+
+        SELECT 'Orden cancelada correctamente.' AS result;
+    END IF;
+END
 
