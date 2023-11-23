5718768d4af48aaf42c9d08f57c844e7815cc98d
Lt-Sites <42856882+Lt-Sites@users.noreply.github.com>
2023-11-22 22:53:57 -0600

    Update AddBooking Task 1.sql

--- AddBooking Task 1.sql
+++ AddBooking Task 1.sql
@@ -1,4 +1,16 @@
-CREATE PROCEDURE AddBooking (IN BookingID INT, IN CustomerID INT, IN TableNumber INT, IN BookingDate DATE)
+CREATE PROCEDURE AddBooking (IN p_BookingID INT, IN p_CustomerID INT, IN p_TableNumber INT, IN p_BookingDate DATE)
 BEGIN
-INSERT INTO booking (bookingid, customerid, tablenumber, date) VALUES (BookingID, CustomerID, TableNumber, BookingDate); 
+    DECLARE error_message VARCHAR(255);
+    
+    -- Verificar si la reserva ya existe
+    IF EXISTS (SELECT 1 FROM booking WHERE bookingid = p_BookingID) THEN
+        SET error_message = 'La reserva ya existe.';
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = error_message;
+    ELSE
+        -- Insertar la reserva
+        INSERT INTO booking (bookingid, customerid, tablenumber, date)
+        VALUES (p_BookingID, p_CustomerID, p_TableNumber, p_BookingDate);
+        
+        SELECT 'Reserva agregada correctamente.' AS result;
+    END IF;
 END
