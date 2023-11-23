2b5b7114b0de619dd815111f23accfa51d18d4b0
Lt-Sites <42856882+Lt-Sites@users.noreply.github.com>
2023-11-22 22:55:29 -0600

    Update CancelBooking Task 3.sql

--- CancelBooking Task 3.sql
+++ CancelBooking Task 3.sql
@@ -1,4 +1,15 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `CancelBooking`(IN BookingID INT)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `CancelBooking`(IN p_BookingID INT)
 BEGIN
-DELETE FROM booking WHERE booking_id = BookingID;
+    DECLARE error_message VARCHAR(255);
+
+    -- Verificar si la reserva existe antes de intentar cancelarla
+    IF NOT EXISTS (SELECT 1 FROM booking WHERE booking_id = p_BookingID) THEN
+        SET error_message = 'La reserva no existe.';
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = error_message;
+    ELSE
+        -- Eliminar la reserva si existe
+        DELETE FROM booking WHERE booking_id = p_BookingID;
+
+        SELECT 'Reserva cancelada correctamente.' AS result;
+    END IF;
 END
