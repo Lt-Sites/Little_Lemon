e8766dbc85b8f66218137307a60ae8ec673e31ba
Lt-Sites <42856882+Lt-Sites@users.noreply.github.com>
2023-11-22 22:57:46 -0600

    Update UpdateBooking Task 2.sql

--- UpdateBooking Task 2.sql
+++ UpdateBooking Task 2.sql
@@ -1,4 +1,15 @@
-CREATE PROCEDURE UpdateBooking (IN BookingID INT, IN BookingDate DATE)
+CREATE PROCEDURE UpdateBooking (IN p_BookingID INT, IN p_BookingDate DATE)
 BEGIN
-UPDATE booking SET date = BookingDate WHERE booking_id = BookingID; 
+    DECLARE error_message VARCHAR(255);
+
+    -- Verificar si la reserva existe antes de intentar actualizarla
+    IF NOT EXISTS (SELECT 1 FROM booking WHERE booking_id = p_BookingID) THEN
+        SET error_message = 'La reserva no existe.';
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = error_message;
+    ELSE
+        -- Actualizar la fecha de la reserva
+        UPDATE booking SET date = p_BookingDate WHERE booking_id = p_BookingID;
+
+        SELECT 'Fecha de reserva actualizada correctamente.' AS result;
+    END IF;
 END
