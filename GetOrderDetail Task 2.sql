a6238e11f321267c429a7ab3217d45a5ca633b4e
Lt-Sites <42856882+Lt-Sites@users.noreply.github.com>
2023-11-22 22:57:12 -0600

    Update GetOrderDetail Task 2.sql

--- GetOrderDetail Task 2.sql
+++ GetOrderDetail Task 2.sql
@@ -1,3 +1,13 @@
-PREPARE GetOrderDetail from 'SELECT OrderID, Quantity, TotalCost from Orders where OrderID=?'
-SET @id = 1;
-EXECUTE GetOrderDetail USING @id;
+CREATE PROCEDURE GetOrderDetail(IN p_OrderID INT)
+BEGIN
+    SET @sql_query = 'SELECT OrderID, Quantity, TotalCost FROM Orders WHERE OrderID = ?';
+
+    -- Preparar la consulta
+    PREPARE stmt FROM @sql_query;
+
+    -- Ejecutar la consulta con el parámetro
+    EXECUTE stmt USING p_OrderID;
+
+    -- Liberar la consulta preparada
+    DEALLOCATE PREPARE stmt;
+END
