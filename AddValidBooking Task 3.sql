628187750539f3c83b744bfeec0052f0e4d231f8
Lt-Sites <42856882+Lt-Sites@users.noreply.github.com>
2023-11-22 22:54:55 -0600

    Update AddValidBooking Task 3.sql

--- AddValidBooking Task 3.sql
+++ AddValidBooking Task 3.sql
@@ -1,14 +1,21 @@
-CREATE PROCEDURE AddValidBooking (IN BookingDate DATE, IN TableNumber INT)
+CREATE PROCEDURE AddValidBooking (IN p_BookingDate DATE, IN p_TableNumber INT)
+BEGIN
+    DECLARE error_message VARCHAR(255);
 
-START TRANSACTION;
+    START TRANSACTION;
 
-SELECT BookingDate, TableNumber
-WHERE exists (SELECT * from Booking where Date = BookingDate and TableNumber = TableNumber)
+    -- Verificar si ya existe una reserva para la fecha y la mesa proporcionadas
+    IF EXISTS (SELECT 1 FROM booking WHERE date = p_BookingDate AND table_number = p_TableNumber) THEN
+        SET error_message = 'Ya hay una reserva para esa fecha y mesa.';
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = error_message;
+    ELSE
+        -- Insertar la reserva si no existe una reserva para la fecha y mesa proporcionadas
+        INSERT INTO booking (date, table_number)
+        VALUES (p_BookingDate, p_TableNumber);
 
-	INSERT INTO booking (date, table_number)
-	VALUES (BookingDate, TableNumber);
-    
-	COMMIT;
-    
+        SELECT 'Reserva agregada correctamente.' AS result;
+    END IF;
+
+    COMMIT;
     
 
